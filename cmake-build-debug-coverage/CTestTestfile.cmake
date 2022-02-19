895dd1c20fbfa83fc413e4262660b3cc5fa60b4c
idigitallegacy <99473127+idigitallegacy@users.noreply.github.com>
2022-02-19 19:47:29 +0300

    Delete cmake-build-debug-coverage directory

--- cmake-build-debug-coverage/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/labs_prog_cpp/lab1
-# Build directory: /mnt/d/labs_prog_cpp/lab1/cmake-build-debug-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/d/labs_prog_cpp/lab1/cmake-build-debug-coverage/main_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- cmake-build-debug-coverage/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/labs_prog_cpp/lab1/cmake-build-debug-coverage/_deps/googletest-src/googlemock
-# Build directory: /mnt/d/labs_prog_cpp/lab1/cmake-build-debug-coverage/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
